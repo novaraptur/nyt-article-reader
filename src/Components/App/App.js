8a5a0f5cbee27b8a658ace97574e6213124ec511
Dean Cook <deancook371@gmail.com>
2021-09-21 10:30:21 -0700

    Add Search component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import Header from '../Header/Header';
+import Search from '../Search/Search';
 
 const App = () => {
   return (
     <main>
       <Header />
+      <Search />
     </main>
   );
 }
