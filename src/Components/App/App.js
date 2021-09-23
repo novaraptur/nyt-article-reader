d60a90b969120867e01c9df9007a7c9981a69c4e
Dean Cook <deancook371@gmail.com>
2021-09-22 19:57:01 -0700

    Add API call and install dotenv

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,29 @@
+import React, { useEffect, useState, useCallback } from 'react';
 import './App.css';
 
 import Header from '../Header/Header';
 import ArticlesList from '../ArticlesList/ArticlesList';
 import Search from '../Search/Search';
 
+import { getArticles } from '../../api_calls';
+
+const dotenv = require('dotenv').config();
+
 const App = () => {
+  const [allArticles, setAllArticles] = useState({});
+
+  useEffect(() => {
+    getArticles()
+      .then((data) => {
+        setAllArticles(data)
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, []);
+
+  console.log(allArticles);
+
   return (
     <main>
       <Header />
