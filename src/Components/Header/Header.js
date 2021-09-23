223a9ae6a4f489a9c2bc07767be852d5a7f804bd
Dean Cook <deancook371@gmail.com>
2021-09-22 20:00:16 -0700

    Update header content

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,8 @@ import './Header.css';
 const Header = () => {
   return (
     <header>
-      <h1>READ</h1>
+      <h1>NYT Reader</h1>
+      <h3>Top Articles From The New York Times</h3>
     </header>
   );
 }
