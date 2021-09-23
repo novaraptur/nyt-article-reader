de1a2d3503b3a3ae3275d0e8f706a5de6de06aae
Dean Cook <deancook371@gmail.com>
2021-09-22 21:33:32 -0700

    Add nav link to article cards

--- src/Components/ArticleCard/ArticleCard.js
+++ src/Components/ArticleCard/ArticleCard.js
@@ -1,11 +1,13 @@
 import './ArticleCard.css';
 
+import { NavLink, Redirect } from 'react-router-dom';
+
 const ArticleCard = ({article}) => {
   return (
-    <article className="article-card">
+    <NavLink to={`/${article.created_date}`} id={article.created_date} className="article-card">
       <h4>{article.title}</h4>
       <p>{article.abstract}</p>
-    </article>
+    </NavLink>
   );
 }
 
